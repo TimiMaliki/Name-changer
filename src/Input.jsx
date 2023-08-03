59a3bd6a98cb156d97e97b15ccb8128ef3e84771
TimiMaliki <malikitimi@gmail.com>
2023-08-03 04:24:30 +0100

    start

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import colorNames from 'colornames'
 
-const Input = ({colorValue,setColorValue, setHexValue }) => {
+const Input = ({colorValue,setColorValue, setHexValue, isDarkText, setIsDarkText }) => {
   return (
   <form action="" onSubmit={(e) => e.preventDefault()}>
     <label htmlFor="">Add Color Name</label>
@@ -13,8 +13,14 @@ const Input = ({colorValue,setColorValue, setHexValue }) => {
     onChange={(e) => {setColorValue(e.target.value)
     setHexValue(colorNames(e.target.value))
     }}
-      
     />
+
+    <button 
+    type='button'
+    onClick={() => setIsDarkText(!isDarkText)}
+    >
+        Toggle Text Color
+    </button>
   </form>
   )
 }
--- src/Sqaure.jsx
+++ src/Sqaure.jsx
@@ -1,10 +1,14 @@
-const Sqaure = ({colorValue,hexValue}) => {
+const Sqaure = ({colorValue,hexValue,isDarkText}) => {
   return (
     <section
     className="square"
-    style={{backgroundColor:colorValue}}
+    style={{
+        backgroundColor:colorValue,
+      color: isDarkText ? "#000" : "#FFF" 
+    }}
     >
-       <p>{colorValue ? colorValue : "Empty Value"}</p>
+       <p>{colorValue ? colorValue : "Empty Value"}
+       </p>
        <p>{hexValue ? hexValue : null}</p>
     </section>
   )
